b49155026bf388d7b59c8580da03f15c9d45bb35
javila35 <josepharthuravila@gmail.com>
2020-09-01 17:48:16 -0700

    Test deploy with useEffect update.

--- src/components/blog/BlogPosts.js
+++ src/components/blog/BlogPosts.js
@@ -1,49 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import BlogCard from './BlogCard';
 import { graphql, useStaticQuery } from 'gatsby';
-
-/* 
-, { useState, useEffect }
-const [allPosts, setPosts] = useState(queryData.allDevArticles.edges)
-
-useEffect(() => {
-        fetch('https://www.joeavila.dev/___graphql/', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ 
-                query: `{
-                    allDevArticles {
-                        edges {
-                            node {
-                                article {
-                                    id
-                                    social_image
-                                    title
-                                    url
-                                    readable_publish_date
-                                }
-                            }
-                        }
-                    }
-                }`
-            })
-        })
-        .then(res => res.json())
-        .then(res => {
-            if (checkNewPosts(res.data.allDevArticles.edges)) {
-                setPosts(res.data.allDevArticles.edges)
-            }
-        })
-    })
-
-    const checkNewPosts = (data) => {
-        if (allPosts.length !== data.length) {
-            return true
-        };
-        return false;
-    }
-
-*/
 
 const BlogPosts = () => {
     const queryData = useStaticQuery(graphql`
@@ -64,7 +21,44 @@ const BlogPosts = () => {
     }
   `);
 
-    const allPosts = queryData.allDevArticles.edges;
+  const [allPosts, setPosts] = useState(queryData.allDevArticles.edges)
+
+  useEffect(() => {
+          fetch('https://www.joeavila.dev/___graphql/', {
+              method: 'POST',
+              headers: { 'Content-Type': 'application/json' },
+              body: JSON.stringify({ 
+                  query: `{
+                      allDevArticles {
+                          edges {
+                              node {
+                                  article {
+                                      id
+                                      social_image
+                                      title
+                                      url
+                                      readable_publish_date
+                                  }
+                              }
+                          }
+                      }
+                  }`
+              })
+          })
+          .then(res => res.json())
+          .then(res => {
+              if (checkNewPosts(res.data.allDevArticles.edges)) {
+                  setPosts(res.data.allDevArticles.edges)
+              }
+          })
+      })
+  
+      const checkNewPosts = (data) => {
+          if (allPosts.length !== data.length) {
+              return true
+          };
+          return false;
+      }
 
     const renderPosts = () => {
         return allPosts.map((post, index) => {
