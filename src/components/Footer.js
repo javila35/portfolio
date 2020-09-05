e18adde295f38f4dfe9d3273973440bba0ce92f0
Joe Avila <58503695+javila35@users.noreply.github.com>
2020-09-04 20:31:34 -0700

    Update Footer.js
    updating twitter handle

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,12 +6,12 @@ const Footer = () => (
     <div className="inner">
       <ul className="icons">
         <li>
-          <a href="https://twitter.com/CoderJoe206" className="icon fa-twitter" target="_blank" rel="noreferrer" aria-label="Twitter">
+          <a href="https://twitter.com/joeaviladev/" className="icon fa-twitter" target="_blank" rel="noreferrer" aria-label="Twitter">
             <span className="label">Twitter</span>
           </a>
         </li>
         <li>
-          <a href="https://github.com/javila35" className="icon fa-github" target="_blank" rel="noreferrer" aria-label="Github">
+          <a href="https://github.com/javila35/" className="icon fa-github" target="_blank" rel="noreferrer" aria-label="Github">
             <span className="label">Github</span>
           </a>
         </li>
