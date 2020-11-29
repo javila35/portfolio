e2ee12161619f9b0974b9936973db68335f4cce5
javila35 <josepharthuravila@gmail.com>
2020-11-28 21:34:02 -0800

    Added music to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import MusicCard from '../components/404/MusicCard';
 import Layout from '../components/layout';
 import SEO from '../components/SEO'
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO 
+    <SEO
       title="Joe Avila || Lost & Found"
     />
-    
+
     <div id="main">
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <h1>You found a 404 page</h1>
+      <p>Here's the last song I listened too:</p>
+      <MusicCard />
     </div>
   </Layout>
 );
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import SEO from '../components/SEO';
 const HomeIndex = () => {
   return (
     <Layout>
-      <SEO 
+      <SEO
         title="Joe Avila || Web Developer"
       />
 
@@ -19,12 +19,12 @@ const HomeIndex = () => {
           </header>
           <p>
             I'm a web developer based in Seattle, WA. I'm currently working with <a href="https://www.collineargroup.com/">Collinear Group</a> as a full stack software engineer.
-            <br/>
-            <br/>
+            <br />
+            <br />
             I work well in a team environment where I get to practice my listening skills and engage in thoughtful conversation.
             In leadership roles my priority is to understand the strengths and goals of individual members to create an environment where everyone is challenged and positioned to succeed.
             My diverse work history has given me the ability to quickly adapt to change whether it's a customer changing their mind or an operation system being updated on the fly.
-            I have a passion for connecting the dots between teams and individuals, and can use my critical thinking to identify high priority issues that require a sense of urgency. 
+            I have a passion for connecting the dots between teams and individuals, and can use my critical thinking to identify high priority issues that require a sense of urgency.
           </p>
         </section>
 
@@ -35,16 +35,16 @@ const HomeIndex = () => {
 
         <section id="three">
           <h2>Get In Touch</h2>
-            <div className="4u 12u$(small)">
-              <ul className="labeled-icons">
-                <li>
-                  <h3 className="icon fa-envelope-o">
-                    <span className="label">Email</span>
-                  </h3>
-                  <a href="mailto:joeavilacodes@gmail.com">joeavilacodes@gmail.com</a>
-                </li>
-              </ul>
-            </div>
+          <div className="4u 12u$(small)">
+            <ul className="labeled-icons">
+              <li>
+                <h3 className="icon fa-envelope-o">
+                  <span className="label">Email</span>
+                </h3>
+                <a href="mailto:joeavilacodes@gmail.com">joeavilacodes@gmail.com</a>
+              </li>
+            </ul>
+          </div>
         </section>
       </div>
     </Layout>
