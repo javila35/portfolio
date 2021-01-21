fdf7927e208b9d84f4fc4236ea71ca912a4d5f70
Joe Avila <josepharthuravila@gmail.com>
2021-01-20 21:05:22 -0800

    Add preload javascript

---
+++ src/index.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementsByTagName("body")[0].classList.remove("preload");
+});
+
+document.removeEventListener("DOMContentLoaded");
