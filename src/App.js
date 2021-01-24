229b75554851efccfea04ffa6ebc524a6c332248
Joe Avila <josepharthuravila@gmail.com>
2021-01-24 11:50:21 -0800

    Updates App structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import * as React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import { Nav } from "./components";
+import { Home, Blog } from "./views";
 
 export const App = () => {
     return (
         <div>
-            Hi,
+            <BrowserRouter>
+                <Route path="/" exact component={Home} />
+                <Route path="/blog" component={Blog} />
+                <Nav />
+            </BrowserRouter>
         </div>
     )
 };
