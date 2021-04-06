5fff82e5f954faf6b6626a301c786e062ab2339b
javila35 <josepharthuravila@gmail.com>
2021-04-06 13:22:50 -0700

    Add resume locally

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,31 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-    mode: "development",
-    entry: "./src/index.js",
-    devServer: {
-        contentBase: "./dist",
-    },
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "dist"),
-    },
-    plugins: [new HtmlWebpackPlugin({
-        template: "src/index.html"
-    })],
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            }
-        ]
-    }
-}
+  mode: "development",
+  entry: "./src/index.js",
+  devServer: {
+    contentBase: "./dist",
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "src/assets", to: "assets" }],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
